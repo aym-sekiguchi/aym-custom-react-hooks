8d3bcd4334f45fafef0470523d182718d6e31a52
Ayami sekiguchi <aym.jun.k@gmail.com>
2025-05-12 09:53:07 +0900

    chore: GitHub Pages 用設定追加とリンクパスの修正
    - Vite の `base` 設定を `./` に変更
    - tsx内のリンクパスを相対パスに修正
    - `gh-pages` パッケージを devDependencies に追加
    - `package.json` に `deploy` スクリプトを追加（`gh-pages -d dist`)

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,8 +8,8 @@ import "@/styles/globals.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Layout>
-      <PageTitle>Hooks</PageTitle>
-      <Card href="/useToggle/">
+      <PageTitle>Hooks1</PageTitle>
+      <Card href="./useToggle/">
         <CardHeader>useToggle</CardHeader>
         <CardDescription>
           <strong>useToggle</strong> は、真偽値の状態を切り替えるカスタムフックです。
