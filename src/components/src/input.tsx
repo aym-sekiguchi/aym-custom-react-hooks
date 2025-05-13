b48893aa19cf54428b76c24268b60ef53e600e30
Ayami sekiguchi <aym.jun.k@gmail.com>
2025-05-13 12:45:06 +0900

    refactor: Input コンポーネントから不要な children を削除

--- src/components/src/input.tsx
+++ src/components/src/input.tsx
@@ -1,14 +1,12 @@
-export function Input(props: { children?: React.ReactNode } & React.HTMLProps<HTMLInputElement>) {
+export function Input(props: React.HTMLProps<HTMLInputElement>) {
   /* === props === */
-  const { children, className, ...rest } = props;
+  const { className, ...rest } = props;
 
   /* === return === */
   return (
     <input
       {...rest}
       className={`border border-zinc-500 rounded-md px-3 focus-visible:outline-0 focus-visible:border-blue-500 focus-visible:border-2 h-[2em] ${className}`}
-    >
-      {children}
-    </input>
+    />
   );
 }
